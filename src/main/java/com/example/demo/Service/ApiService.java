a54f791dc07a81cc79f1e121c136f815beb49a8e
vayisha <vayishalini@gmail.com>
2023-02-26 15:58:23 +0530

    portal5

---
+++ src/main/java/com/example/demo/Service/ApiService.java
@@ -0,0 +1,29 @@
+package com.example.demo.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.Models.book;
+import com.example.demo.Repositories.bookrepo;
+
+
+
+public class ApiService {
+	@Autowired
+	bookrepo repository;
+		public Optional<book> getbook(int id){
+			return repository.findById(id);
+		}
+
+		public String updateDetails(book b) {
+			repository.save(b);
+			return "updated";
+		}
+
+		public String deleteDetails(int id) {
+			repository.deleteById(id);
+			return "Id deleted";
+		}
+
+}
