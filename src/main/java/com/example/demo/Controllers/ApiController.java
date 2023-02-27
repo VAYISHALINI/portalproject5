043e40db3d25ddac7133a1ea35da8880556cb36f
vayisha <vayishalini@gmail.com>
2023-02-27 14:22:22 +0530

    poratl5

--- src/main/java/com/example/demo/Controllers/ApiController.java
+++ src/main/java/com/example/demo/Controllers/ApiController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.Controllers;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/example/demo/Service/ApiService.java
+++ src/main/java/com/example/demo/Service/ApiService.java
@@ -3,12 +3,13 @@ package com.example.demo.Service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.Models.book;
 import com.example.demo.Repositories.bookrepo;
 
 
-
+@Service
 public class ApiService {
 	@Autowired
 	bookrepo repository;
